c9ac7c586bcd8d3e38c8255e95f252239fc95d3e
Subsistance <subsistancex@gmail.com>
2024-01-19 03:42:50 +0400

    small refactoring - added separate parse methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,9 +7,17 @@ class ProductPage(BasePage):
         link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         link.click()
         
+    def parse_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        return product_name
+        
+    def parse_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        return product_price
+        
     def verify_added_product_name(self):
         try:
-            product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+            product_name = self.parse_product_name()
             alert_product_name = self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_NAME)
             assert product_name.text in alert_product_name.text, "Product name is different"
         except NoSuchElementException as e:
@@ -17,7 +25,7 @@ class ProductPage(BasePage):
             
     def verify_total_basket_price(self):
         try:
-            product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+            product_price = self.parse_product_price()
             basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL)
             assert basket_total.text == product_price.text, "Product price and basket total are different"
         except NoSuchElementException as e:
--- test_product_page.py
+++ test_product_page.py
@@ -2,10 +2,12 @@ from selenium.webdriver.common.by import By
 from pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser,link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
+    page.parse_product_name()
+    page.parse_product_price()
     page.verify_added_product_name()
     page.verify_total_basket_price()
