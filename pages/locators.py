da8cdf26063c8977610fa519efc494275b3eaded
Subsistance <subsistancex@gmail.com>
2024-01-21 03:28:47 +0400

    additional tests

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,5 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -15,3 +12,7 @@ class ProductPageLocators():
     BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner>p>strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main>.price_color")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1)")
+    
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,10 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
+import time
+
 
                              
 # def test_guest_can_add_product_to_basket(browser):
@@ -12,24 +16,38 @@ import pytest
     # page.parse_product_price()
     # page.verify_added_product_name()
  
-@pytest.mark.xfail 
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-    page = ProductPage(browser,link)
-    page.open()
-    page.add_to_basket()
-    page.should_not_be_success_message()
+# @pytest.mark.xfail 
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    # link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    # page = ProductPage(browser,link)
+    # page.open()
+    # page.add_to_basket()
+    # page.should_not_be_success_message()
     
-def test_guest_cant_see_success_message(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-    page = ProductPage(browser,link)
-    page.open()
-    page.should_not_be_success_message()
+# def test_guest_cant_see_success_message(browser):
+    # link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    # page = ProductPage(browser,link)
+    # page.open()
+    # page.should_not_be_success_message()
 
-@pytest.mark.xfail
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-    page = ProductPage(browser,link)
+# @pytest.mark.xfail
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+    # link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    # page = ProductPage(browser,link)
+    # page.open()
+    # page.add_to_basket()
+    # page.success_message_should_disappear()
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
     page.open()
-    page.add_to_basket()
-    page.success_message_should_disappear()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
